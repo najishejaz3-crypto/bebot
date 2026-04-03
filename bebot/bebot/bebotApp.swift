1dfce5f2c79f7964cce43a2d1c7bd749b10eeb65
najishejaz3-crypto <najishejaz3@gmail.com>
2026-04-03 22:18:32 +0530

    file updated

--- bebot/bebot/bebotApp.swift
+++ bebot/bebot/bebotApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct bebotApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(isLoggedIn: .constant(false))
         }
     }
 }

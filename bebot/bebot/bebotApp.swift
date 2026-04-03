ee5db4d36386def19b37627340238866d8822ff8
najishejaz3-crypto <najishejaz3@gmail.com>
2026-04-03 21:15:39 +0530

    initial commit

---
+++ bebot/bebot/bebotApp.swift
@@ -0,0 +1,17 @@
+//
+//  bebotApp.swift
+//  bebot
+//
+//  Created by abdul karim on 03/04/26.
+//
+
+import SwiftUI
+
+@main
+struct bebotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
